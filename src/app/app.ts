6ddaf8962a1310861a19bc829c24724d5770716d
sumou <zalicsu@gmail.com>
2026-06-11 11:41:38 +0800

    feat: 添加轉場的貼心小提醒

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -7,6 +7,28 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('period-app');
+  protected readonly isLoading = signal(true);
+  protected readonly randomQuote = signal('溫柔擁抱自己的每一個週期與情緒變化。');
+
+  ngOnInit() {
+    // Fetch random quotes from local quotes.json
+    fetch('quotes.json')
+      .then(res => res.json())
+      .then((quotes: string[]) => {
+        if (quotes && quotes.length > 0) {
+          const randomIndex = Math.floor(Math.random() * quotes.length);
+          this.randomQuote.set(quotes[randomIndex]);
+        }
+      })
+      .catch(err => {
+        console.warn('Could not load startup quotes, using default.', err);
+      });
+
+    // 1.5 seconds loading duration
+    setTimeout(() => {
+      this.isLoading.set(false);
+    }, 1500);
+  }
 }
